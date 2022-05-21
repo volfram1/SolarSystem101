0e5073706e0e09c13f5f4a165aeff065eddc21d9
volfram1 <atomczak256@gmail.com>
2022-05-20 23:07:07 -0400

    Solar System Basic Ready

---
+++ src/com/groupProjects/Main.java
@@ -0,0 +1,13 @@
+package com.groupProjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         SolarSystem FactorX=new SolarSystem();
+         FactorX.sun="1";
+         FactorX.moon="4";
+         FactorX.planet="2";
+         FactorX.stars="10000";
+    }
+}
---
+++ src/com/groupProjects/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.groupProjects;
+
+public class SolarSystem {
+    String sun;
+    String moon;
+    String planet;
+    String stars;
+
+}
